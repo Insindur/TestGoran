3eb4eea997598be5654ba516406677fba165b823
Sean Skinner <Sean.Skinner@noroff.no>
2020-09-16 11:01:39 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGoran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
